e9c8c2eff6612fbcc00636c83710faf984cf5c64
ivanbenko <ivan@krafteq.de>
2022-03-28 13:05:00 +0200

    formatting

--- src/dev.ts
+++ src/dev.ts
@@ -5,18 +5,20 @@ import { StreamReader } from "./streamReader";
 const testEndpoint = "streams.proxima.one:443";
 const client = new StreamClient(testEndpoint);
 
-let lastEventTs = -1;
 async function main() {
-  const streamReader = new StreamReader(client, "v4.domain-events.polygon-mumbai.mangrove.streams.proxima.one");
+  const streamReader = new StreamReader(
+    client,
+    "v4.domain-events.polygon-mumbai.mangrove.streams.proxima.one"
+  );
 
-  while(true) {
+  while (true) {
     const messages = await streamReader.tryRead(1000, 10 * 60 * 1000);
-    for(const message of messages) {
+    for (const message of messages) {
       console.log(message.id);
     }
   }
 }
 
-main().catch(err => {
+main().catch((err) => {
   console.error(err);
 });
--- src/streamReader.ts
+++ src/streamReader.ts
@@ -1,5 +1,15 @@
 import { StreamClient, StreamMessage } from "./client";
-import { bufferTime, catchError, filter, firstValueFrom, Observable, take, timeout, TimeoutError, of } from "rxjs";
+import {
+  bufferTime,
+  catchError,
+  filter,
+  firstValueFrom,
+  Observable,
+  take,
+  timeout,
+  TimeoutError,
+  of,
+} from "rxjs";
 
 // temporary solution, will be generalized using common proxima-streaming package once open sourced
 export class StreamReader {
@@ -8,34 +18,43 @@ export class StreamReader {
   public constructor(
     private readonly client: StreamClient,
     private readonly streamId: string,
-    private lastMessageId?: string,
-  ) {
-  }
+    private lastMessageId?: string
+  ) {}
 
-  public async tryRead(maxCount: number, timeoutMs: number): Promise<StreamMessage[]> {
-    const storedMessages = (await this.client.getNextMessages(this.streamId, {
-      latest: this.lastMessageId,
-      messageCount: maxCount
-    })).messagesList;
+  public async tryRead(
+    maxCount: number,
+    timeoutMs: number
+  ): Promise<StreamMessage[]> {
+    const storedMessages = (
+      await this.client.getNextMessages(this.streamId, {
+        latest: this.lastMessageId,
+        messageCount: maxCount,
+      })
+    ).messagesList;
 
     if (storedMessages.length > 0) {
       this.lastMessageId = storedMessages[storedMessages.length - 1].id;
       return storedMessages;
     }
 
-    const liveMessagesStream = this.client.streamMessages(this.streamId, { latest: this.lastMessageId }).pipe(
-      timeout({ first: timeoutMs }),
-      bufferTime(this.bufferTimeInterval, this.bufferTimeInterval, maxCount),
-      filter(x => x.length > 0),
-      take(1),
-    );
+    const liveMessagesStream = this.client
+      .streamMessages(this.streamId, { latest: this.lastMessageId })
+      .pipe(
+        timeout({ first: timeoutMs }),
+        bufferTime(this.bufferTimeInterval, this.bufferTimeInterval, maxCount),
+        filter((x) => x.length > 0),
+        take(1)
+      );
 
-    const firstMessages = await firstValueFrom(liveMessagesStream.pipe(catchError(err => {
-      if (err instanceof TimeoutError)
-        return of([]);
-      console.debug("streamMessages error occurred", err);
-      return of([]);
-    })));
+    const firstMessages = await firstValueFrom(
+      liveMessagesStream.pipe(
+        catchError((err) => {
+          if (err instanceof TimeoutError) return of([]);
+          console.debug("streamMessages error occurred", err);
+          return of([]);
+        })
+      )
+    );
 
     if (firstMessages.length > 0) {
       this.lastMessageId = firstMessages[firstMessages.length - 1].id;
