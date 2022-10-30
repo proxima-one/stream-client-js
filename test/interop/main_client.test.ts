b1feca691598f69ac55d9f6717b20fbf66381ee2
ivanbianko <ivan@krafteq.de>
2022-10-30 18:06:01 +0100

    fix version, removed tests

--- test/interop/main_client.test.ts
+++ test/interop/main_client.test.ts
@@ -1,73 +1,9 @@
-import { StreamRegistryClient, StreamClient} from "../../src";
 import { strict as assert } from "assert";
-
-const testEndpoint = "https://stream-api.cluster.amur-dev.proxima.one";
-//const testEndpoint = "http://localhost:7000"
-jest.setTimeout(30000)
+import { ProximaStreamClient } from "../../src";
 
 describe("StreamClient", () => {
-  let client: StreamClient;
-  beforeEach(() => {
-    client = new StreamClient({registry: new StreamRegistryClient(testEndpoint)});
-  });
+  jest.setTimeout(30000)
+  const client = new ProximaStreamClient();
 
-  it("should get stream endpoints and data", async () => {
-    jest.setTimeout(30000)
-    const name = "eth-main-blockheader0.new-runtime"
-    const stream =  await client.getStream(name)
-    console.log(stream)
-    expect(stream.metadata).toMatchSnapshot();
-    expect(stream.endpoints).toBeDefined()
-    expect(stream.name).toMatch(name)
-  });
-
-  it("should be able to get single stream, sttreams, and select stream", async () => {
-    jest.setTimeout(30000)
-    const name = "eth-main-blockheader0.new-runtime"
-    const stream =  await client.getStream(name)
-    console.log(stream)
-    expect(stream.metadata).toMatchSnapshot();
-    expect(stream.endpoints).toBeDefined()
-    expect(stream.name).toMatch(name)
-
-    const allStreams = await client.getStreams()
-    expect(allStreams).toMatchSnapshot();
-    expect(allStreams.length).toBeGreaterThan(0);
-  });
-
-  it("should get an offset for a specified height and/or timestamp", async () => {
-    jest.setTimeout(30000)
-    const name = "eth-main-blockheader0.new-runtime"
-    const stream =  await client.getStream(name)
-    expect(stream).toBeDefined()
-    expect(stream.stats).toBeDefined()
-    assert(stream.stats)
-    const height = 1
-    const offset = await client.findOffset(name, Number(height))
-    console.log(offset)
-    expect(offset).toBeDefined()
-    expect(offset).toMatchSnapshot()
-    assert(offset)
-    const events = await client.fetchEvents(name, offset, 100, "next")
-    expect(events).toMatchSnapshot()
-    const newOffset = await client.findOffset(name, Number(height) + 1000)
-    console.log(newOffset)
-    assert(stream.stats.end)
-    await client.findOffset(name, Number(height) + 20)
-    assert(newOffset)
-    const newEvents = await client.fetchEvents(name, newOffset, 100, "next")
-    expect(newEvents).toMatchSnapshot()
-  });
-
-  it("should be able to stream events in pausable stream", async () => {
-    jest.setTimeout(30000)
-    const name = "eth-main-blockheader0.new-runtime"
-    const stream =  await client.getStream(name)
-    expect(stream).toBeDefined()
-    expect(stream.stats).toBeDefined()
-    assert(stream.stats)
-    const height = 1
-    const newOffset = await client.findOffset(name, Number(height))
-    assert(newOffset)
-  });
+  // todo: create new tests for StreamRegistry and StreamClient separately
 });
